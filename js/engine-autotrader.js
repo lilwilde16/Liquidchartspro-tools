a785b13b88e50176bca610dea6dac7d7314450b5
lilwilde16 <lilwilde16@gmail.com>
2026-02-13 17:10:12 -0600

    Create engine-autotrader.js

---
+++ js/engine-autotrader.js
@@ -0,0 +1,7 @@
+(function(){
+    window.ENG = window.ENG || {};
+    window.ENG.AutoTrader = {
+          start: function(){ window.LC.log("Autotrader start (stub)."); },
+          stop: function(){ window.LC.log("Autotrader stop (stub)."); }
+    };
+})();
